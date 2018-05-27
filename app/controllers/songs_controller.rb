7e9263e3922df38da61b3b4d715c254b7144a826
Theo Lauwerier <ssc711@hotmail.fr>
2018-05-27 06:14:18 +0200

    Link image with songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,17 @@
 class SongsController < ApplicationController
   def new
+	@song = Song.new
   end
 
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+  
   def show
 	@song = Song.find(params[:id])
   end
@@ -10,4 +20,7 @@ class SongsController < ApplicationController
 	@songs = Song.all
   end
   
+   def song_params
+      params.require(:song).permit(:name, :artist, :anime, :nbre_vote, :photo)
+   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,5 +3,6 @@ class Song < ApplicationRecord
   validates :artist, presence: true, length: { maximum: 40 }
   validates :anime, presence: true, length: { maximum: 100 }
   validates :nbre_vote, presence: true, numericality: true, inclusion: { in: 0..1000 }
-
+  has_attached_file :photo
+  validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
